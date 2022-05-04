7c454cb093b62d51224de46d6a065ed46ce48310
OtterBoops <me44888@gmail.com>
2022-05-04 19:36:45 +0000

    Last touchup of the day

--- src/www/react/App.jsx
+++ src/www/react/App.jsx
@@ -15,15 +15,15 @@ export default () => (
         <Header />
         <Routes>
           <Route index element = { <Home />} />
-          <Route path = "/login" element = { <Login /> } />
+          <Route path = "login" element = { <Login /> } />
 
           {/* Child routes are inside the stats component  */}
-          <Route path = "/stats/*" element = {<Stats />} /> 
+          <Route path = "stats/*" element = {<Stats />} /> 
 
-          <Route path = "/profile/:username" element = {<Profile />} />
+          <Route path = "profile/:username" element = {<Profile />} />
 
           {/* Default route that redirects back to root */}
-          {/* <Route path="*" element = {<Navigate to ="/" replace />} /> */}
+          <Route path="*" element = {<Navigate to ="/" replace />} />
         </Routes>
         <Footer />
     </BrowserRouter>
--- src/www/react/components/stats/StatsHome.jsx
+++ src/www/react/components/stats/StatsHome.jsx
@@ -4,9 +4,9 @@ import '../../styles/Leaderboard.scss'
 
 const StatsHome = () => {
   return (
-    <>
+    <div className="StatsHome Main">
       Have some stats, bitch. Pick one of the available tabs.
-    </>
+    </div>
   )
 }
 
