657e711cbabdb9e93d3881abebf789602d832953
OtterBoops <me44888@gmail.com>
2022-05-17 20:46:08 +0000

    Finalize initial version

--- src/www/react/components/Shilling.jsx
+++ src/www/react/components/Shilling.jsx
@@ -1,9 +1,78 @@
 import React from "react";
 
+import '../styles/Shilling.scss';
+import Carrd  from '../assets/Carrd.png';
+import Tiktok from '../assets/Tiktok.png';
+import Twitch from '../assets/Twitch.png';
+import Twitter from '../assets/Twitter.png';
+import Youtube from '../assets/Youtube.png';
+import Pixiv from '../assets/Pixiv.png';
+import Patreon from '../assets/Patreon.png';
+
 const Shilling = () => {
   return (
     <main className="Shilling">
-      Shill here
+      <h1>
+        GREMLIN SOUL BUT TWITCH V-BNUI AT HEART
+      </h1> 
+      <div className="ShillCards">
+        <a className="ShillCard" href="https://daishuquee.carrd.co/" target="_blank">
+          <img src={Carrd} alt="Carrd.png" />
+          <p>
+            Commissions
+          </p>
+          <div></div>
+        </a>
+
+        <a className="ShillCard" href="https://twitch.tv/daishutv" target="_blank">
+          <img src={Twitch} alt="Twitch.png" />
+          <p>
+            Twitch
+          </p>
+          <div></div>
+        </a>
+
+        <a className="ShillCard" href="https://www.tiktok.com/@daishutv" target="_blank">
+          <img src={Tiktok} alt="Tiktok.png" />
+          <p>
+            TikTok
+          </p>
+          <div></div>
+        </a>
+
+        <a className="ShillCard" href="https://twitter.com/daishuquee" target="_blank">
+          <img src={Twitter} alt="Twitter.png" />
+          <p>
+            Twitter
+          </p>
+          <div></div>
+        </a>
+
+        <a className="ShillCard" href="https://www.youtube.com/channel/UChTHmPh77XG47kN9uL3mhhg" target="_blank">
+          <img src={Youtube} alt="Youtube.png" />
+          <p>
+            Youtube
+          </p>
+          <div></div>
+        </a>
+
+        <a className="ShillCard" href="https://www.patreon.com/daishu" target="_blank">
+          <img src={Patreon} alt="Patreon.png" />
+          <p>
+            Patreon
+          </p>
+          <div></div>
+        </a>
+
+        <a className="ShillCard" href="https://www.pixiv.net/member.php?id=36112049" target="_blank">
+          <img src={Pixiv} alt="Pixiv.png" />
+          <p>
+            Pixiv
+          </p>
+          <div></div>
+        </a>
+      </div>
+
     </main>
   )
 }
